13de252f1b812f09888afb02814d50808b090339
AlexKharenko <54865466+AlexKharenko@users.noreply.github.com>
2021-04-18 23:33:34 +0300

    fix http problem

--- src/functions/fetch_funcs.js
+++ src/functions/fetch_funcs.js
@@ -1,6 +1,6 @@
 const getWeatherById = async (appId, id) => {
   const res = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${appId}`
+    `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${appId}`
   ).then((response) => response.json());
   console.log(res);
   return res;
